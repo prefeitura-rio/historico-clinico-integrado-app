a2e949a3fc12eaa333cdea5804c35cf1833c028e
Victor Assis <victor_assis@poli.ufrj.br>
2024-08-27 12:36:00 -0300

    fix: :bug: Corrige espaçamento no layout das infos dos cards da timeline

--- src/app/(app)/[cpf]/componentes/tabs/components/timeline/components/timeline-row/components/timeline-card.tsx
+++ src/app/(app)/[cpf]/componentes/tabs/components/timeline/components/timeline-row/components/timeline-card.tsx
@@ -17,7 +17,7 @@ export function TimelineCard({ item }: TimelineCardProps) {
   return (
     <div className="w-full pb-14">
       <Card className="-mt-10 grid grid-cols-1 transition-colors duration-300 hover:bg-gray-300">
-        <div className="col-span-5 grid grid-cols-8 p-[2.25rem]">
+        <div className="col-span-5 grid grid-cols-7 p-[2.25rem]">
           <div className="col-span-2 flex gap-2">
             <MapPin className="h-6 w-6 shrink-0 text-typography-dark-blue" />
             <div className="flex flex-col">
@@ -39,7 +39,7 @@ export function TimelineCard({ item }: TimelineCardProps) {
             </span>
           </div>
 
-          <div className="flex flex-col">
+          <div className="col-span-2 flex flex-col">
             <span className="text-sm font-medium leading-3.5 text-typography-dark-blue">
               Subtipo
             </span>
