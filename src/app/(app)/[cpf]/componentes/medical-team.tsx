86b2afde3cec58e351da484ffefad22043afcdb9
Victor Assis <victor_assis@poli.ufrj.br>
2024-08-28 09:44:10 -0300

    feat: :sparkles: Aprimora comportamento doa seção de médicos

--- src/app/(app)/[cpf]/componentes/medical-team.tsx
+++ src/app/(app)/[cpf]/componentes/medical-team.tsx
@@ -18,23 +18,63 @@ export function MedicalTeam() {
   const cpf = params?.cpf.toString()
 
   const { data: header, isLoading: isHeaderLoading } = usePatientHeader({ cpf })
+  // const { isLoading: isHeaderLoading } = usePatientHeader({ cpf })
   const { data: user, isLoading: isUserLoading } = useUserDetails()
+
+  // For testing
+  // const header = {
+  //   family_clinic: {
+  //     cnes: null,
+  //     name: null,
+  //     phone: null,
+  //   },
+  //   family_health_team: {
+  //     ine_code: null,
+  //     name: 'Nome',
+  //     phone: '(21)99789-4045',
+  //   },
+  //   social_name: null,
+  //   registration_name: 'Victor Registro',
+  //   medical_responsible: [
+  //     {
+  //       name: 'Alexandra Moraes Ribeiro',
+  //       registry: '57A312B3DB1A54B7',
+  //     },
+  //   ],
+  //   nursing_responsible: [
+  //     {
+  //       name: 'Thais Gonzaga',
+  //       registry: '0A495AB14557B6DE',
+  //     },
+  //   ],
+  // }
 
   const expandableButtons = [
     {
       icon: Phone,
-      text: formatPhone(header?.family_clinic.phone || ''),
-      className: 'hover:w-[12.0625rem]',
+      text: header?.family_clinic.phone
+        ? formatPhone(header?.family_clinic.phone)
+        : 'Não possui telefone',
+      className: header?.family_clinic.phone
+        ? 'hover:w-[12.3625rem]'
+        : 'hover:w-[13.4625rem]',
+      disabled: !header?.family_clinic.name || isHeaderLoading,
       title: header?.family_clinic.name,
+      emptyTitle: 'Não há UAP',
       subtitle: 'Unidade de Atenção Primária',
-      copy: true,
-      emptyText: 'A unidade não possui telefone',
     },
     {
       svg: Whatsapp,
-      text: formatPhone(header?.family_health_team.phone || ''),
-      className: 'hover:w-[12.0625rem]',
+      text: header?.family_health_team.phone
+        ? formatPhone(header.family_health_team.phone)
+        : 'Não possui WhatsApp',
+      className: header?.family_health_team.phone
+        ? 'hover:w-[12.3625rem]'
+        : 'hover:w-[13.4625rem]',
+      disabled:
+        !header?.family_health_team.name || isUserLoading || isHeaderLoading,
       title: header?.family_health_team.name,
+      emptyTitle: 'Não existe vínculo',
       subtitle: 'Equipe de Saúde da Família',
       onClick: () => {
         if (header?.family_health_team.phone) {
@@ -42,27 +82,34 @@ export function MedicalTeam() {
             userName: user?.name || '',
             CBO: user?.role || '',
             patientName: header?.social_name || header?.registration_name || '',
-            phoneNumber: header?.family_health_team.phone || '',
+            phoneNumber: header.family_health_team.phone,
           })
         }
       },
-      disabled: isUserLoading,
-      copy: true,
-      emptyText: 'A equipe não possui WhatsApp',
     },
   ]
 
   const popoverButtons = [
     {
-      type: 'popover',
-      list: header?.medical_responsible,
-      title: header?.medical_responsible?.at(0)?.name,
+      list:
+        header?.medical_responsible && header?.medical_responsible.length > 3
+          ? []
+          : header?.medical_responsible,
+      title:
+        header?.medical_responsible && header?.medical_responsible.length > 3
+          ? null
+          : header?.medical_responsible?.at(0)?.name,
       subtitle: 'Médico(a) de referência',
     },
     {
-      type: 'popover',
-      list: header?.nursing_responsible,
-      title: header?.nursing_responsible?.at(0)?.name,
+      list:
+        header?.nursing_responsible && header?.nursing_responsible.length > 3
+          ? []
+          : header?.nursing_responsible,
+      title:
+        header?.nursing_responsible && header?.nursing_responsible.length > 3
+          ? null
+          : header?.nursing_responsible?.at(0)?.name,
       subtitle: 'Enfermeiro(a) de referência',
     },
   ]
@@ -71,16 +118,45 @@ export function MedicalTeam() {
     <div className="z-50 mx-24 mt-10">
       <div className="flex h-14 justify-between">
         {expandableButtons.map((item, index) => (
+          <div key={index} className="flex gap-3">
+            <ExpandableButton
+              Icon={item.icon}
+              svg={item.svg}
+              text={item.text}
+              className={item.className}
+              copy
+              disabled={item.disabled}
+              onClick={item.onClick}
+            />
+            <div className="flex flex-col">
+              {isHeaderLoading ? (
+                <Skeleton className="mt-2 h-3.5 w-40" />
+              ) : (
+                <span
+                  className={cn(
+                    'block pt-2 text-sm leading-[0.875rem]',
+                    item.title
+                      ? 'text-typography-dark-blue'
+                      : 'text-typography-dark-blue/50',
+                  )}
+                >
+                  {item.title || item.emptyTitle}
+                </span>
+              )}
+              <span className="block text-sm text-typography-blue-gray-200">
+                {item.subtitle}
+              </span>
+            </div>
+          </div>
+        ))}
+
+        {popoverButtons.map((item, index) => (
           <div key={index} className="flex">
-            <div className="flex gap-3">
-              <ExpandableButton
-                Icon={item.icon}
-                svg={item.svg}
-                text={item.text || ''}
-                className={item?.className}
-                onClick={item.onClick}
-                copy={!!item?.copy}
-                disabled={item.disabled || !item.text}
+            <div key={index} className="flex gap-3">
+              <MedicalTeamPopover
+                list={item.list?.map((item) => item.name) || []}
+                title={item.subtitle || ''}
+                disabled={isHeaderLoading}
               />
               <div className="flex flex-col">
                 {isHeaderLoading ? (
@@ -94,31 +170,7 @@ export function MedicalTeam() {
                         : 'text-typography-dark-blue/50',
                     )}
                   >
-                    {item.title || item.emptyText}
-                  </span>
-                )}
-
-                <span className="block text-sm text-typography-blue-gray-200">
-                  {item.subtitle}
-                </span>
-              </div>
-            </div>
-          </div>
-        ))}
-        {popoverButtons.map((item, index) => (
-          <div key={index} className="flex">
-            <div key={index} className="flex gap-3">
-              <MedicalTeamPopover
-                list={item.list?.map((item) => item.name) || []}
-                title={item.subtitle || ''}
-                disabled={isHeaderLoading}
-              />
-              <div className="flex flex-col">
-                {isHeaderLoading ? (
-                  <Skeleton className="mt-2 h-3.5 w-40" />
-                ) : (
-                  <span className="block pt-2 text-sm leading-[0.875rem] text-typography-dark-blue">
-                    {item.title}
+                    {item.title || 'Não possui'}
                   </span>
                 )}
                 <span className="block text-sm text-typography-blue-gray-200">
