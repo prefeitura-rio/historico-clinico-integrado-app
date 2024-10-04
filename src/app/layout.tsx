ee0aa565db61e1c277f636ac11f5e3af6fc30196
Victor Assis <victor_assis@poli.ufrj.br>
2024-10-04 15:41:59 -0300

    chore: :chart_with_upwards_trend: Implementa integração com Hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,22 @@ export default function RootLayout({
           strategy="beforeInteractive"
           src={`https://www.google.com/recaptcha/api.js?render=${config.captchaSiteKey}`}
         />
+        <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5159759,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
         <TooltipProvider delayDuration={400}>
           <Toaster duration={4000} />
           {children}
