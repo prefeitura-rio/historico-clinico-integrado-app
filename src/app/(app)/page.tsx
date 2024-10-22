17b31f3e565f353080acbe06bb92320b5c0faa74
Victor Assis <victor_assis@poli.ufrj.br>
2024-10-22 09:15:21 -0300

    feat: :sparkles: Implementa diferentes títulos de página

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next'
+
 import { CPFSearch } from './components/cpf-search'
 import { UserNotice } from './components/cpf-search/components/user-notice'
+
+export const metadata: Metadata = {
+  title: 'Histórico Clínico Integrado | Buscar por CPF',
+  description: 'Prefeitura do Rio de Janeiro',
+}
 
 export default function Home() {
   return (
--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/utils/auth'
+
+export const metadata: Metadata = {
+  title: 'Histórico Clínico Integrado | Login',
+  description: 'Prefeitura do Rio de Janeiro',
+}
 
 export default function AuthLayout({
   children,
