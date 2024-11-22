434e6faf31d2798ad7198a2bf126970fe33dc7bb
Victor Assis <victor_assis@poli.ufrj.br>
2024-11-22 09:57:26 -0300

    feat: :lipstick: Implementa header na tela de busca por CPF

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next'
+import Image from 'next/image'
+
+import logoPrefeituraSaude from '@/assets/logo-prefeitura-saude.png'
 
 import { CPFSearch } from './components/cpf-search'
 import { ErrorToast } from './components/error-toast/error-toast'
@@ -13,12 +16,34 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="mx-auto flex min-h-screen max-w-screen-2xl">
-      <div className="relative flex h-screen w-full min-w-screen-sm flex-col items-center justify-center gap-10">
-        <CPFSearch />
-        <UserNotice />
-        <ErrorToast />
+      <div className="flex h-screen w-full min-w-screen-sm flex-col items-center justify-center gap-4">
+        {/* Header */}
+        <div className="relative flex h-32 w-full items-center justify-center gap-3 p-6">
+          <div className="flex items-center gap-12">
+            <Image
+              src={logoPrefeituraSaude}
+              alt="Prefeitura do Rio de Janeiro"
+              className="h-16 w-auto"
+            />
+            <div className="">
+              <span className="block text-xl font-medium leading-5 text-typography-dark-blue">
+                Histórico Clínico Integrado
+              </span>
+            </div>
+          </div>
+
+          <div className="absolute right-0 top-0 flex h-full items-center p-6">
+            <LogoutButton />
+          </div>
+        </div>
+
+        {/* Main */}
+        <div className="relative flex h-screen w-full min-w-screen-sm flex-col items-center justify-center gap-10">
+          <CPFSearch />
+          <UserNotice />
+          <ErrorToast />
+        </div>
       </div>
-      <LogoutButton />
     </div>
   )
 }
