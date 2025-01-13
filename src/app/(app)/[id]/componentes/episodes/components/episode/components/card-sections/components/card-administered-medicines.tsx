7752146280dbc0d2ef250b603e8182b4ac924f94
Victor Assis <victor_assis@poli.ufrj.br>
2025-01-13 15:30:00 -0300

    feat: :sparkles: Implementa filtro de data no modal de medicamentos administrados

--- src/app/(app)/[id]/componentes/episodes/components/episode/components/card-sections/components/card-administered-medicines.tsx
+++ src/app/(app)/[id]/componentes/episodes/components/episode/components/card-sections/components/card-administered-medicines.tsx
@@ -1,8 +1,12 @@
 import { formatDate } from 'date-fns'
-import { Filter, X } from 'lucide-react'
+import { ptBR } from 'date-fns/locale'
+import { CalendarIcon, Filter, X } from 'lucide-react'
+import { useMemo, useState } from 'react'
+import type { DateRange } from 'react-day-picker'
 
 import { HTMLWrapper } from '@/components/custom-ui/html-wrapper'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import {
   Dialog,
   DialogClose,
@@ -11,6 +15,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { Separator } from '@/components/ui/separator'
 import { cn } from '@/lib/utils'
 
@@ -24,8 +33,44 @@ interface CardCIDSectionProps {
 export function CardAdministeredMedicinesSection({
   medicines,
 }: CardCIDSectionProps) {
-  const groups = Object.groupBy(medicines, ({ date }) => date)
-  const mostRecent = Object.entries(groups).at(0)
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: undefined,
+  })
+  const groups = useMemo(
+    () => Object.groupBy(medicines, ({ date }) => date),
+    [medicines],
+  )
+  const mostRecent = useMemo(() => Object.entries(groups).at(0), [groups])
+
+  const data = useMemo(() => Object.entries(groups), [groups])
+  const filteredData = useMemo(() => {
+    // date?.from && !date?.to ? data.filter((item) => new Date(item[0]).getTime() === date.from?.getTime()) :
+
+    // If only one date is selected, filter the data to show only the selected date
+    if (date?.from && !date?.to) {
+      const from = date.from
+
+      return data.filter((item) => {
+        const cur = new Date(item[0])
+        cur.setHours(0, 0, 0, 0)
+
+        return cur.getTime() === from.getTime()
+      })
+    }
+    // If a range of dates is selected, filter the data to show only the dates in the range
+    else if (date?.from && date?.to) {
+      const from = date.from
+      const to = date.to
+
+      return data.filter((item) => {
+        const cur = new Date(item[0])
+        cur.setHours(0, 0, 0, 0)
+        console.log({ cur, from, to })
+
+        return cur.getTime() >= from.getTime() && cur.getTime() <= to.getTime()
+      })
+    } else return data
+  }, [data, date?.from, date?.to])
 
   return (
     <div
@@ -70,7 +115,7 @@ export function CardAdministeredMedicinesSection({
         </div>
       )}
 
-      {Object.entries(groups).length > 1 && (
+      {data.length > 1 && (
         <div>
           <Dialog>
             <DialogTrigger asChild>
@@ -84,32 +129,80 @@ export function CardAdministeredMedicinesSection({
             </DialogTrigger>
             <DialogContent
               closeButton={false}
-              className="max-h-[80%] max-w-[80%] overflow-y-auto"
+              className="flex h-[80%] max-w-[80%] flex-col items-start overflow-y-scroll"
             >
               <DialogClose asChild>
                 <Button
-                  className="absolute right-4 top-4 size-auto border p-2.5"
+                  className="absolute right-2 top-4 size-auto border p-2.5"
                   variant="secondary"
                   size="icon"
                 >
                   <X className="size-4 text-typography-dark-blue" />
                 </Button>
               </DialogClose>
-              <Button
-                className="absolute right-16 top-4 size-auto p-2.5"
-                variant="outline"
-                size="icon"
-              >
-                <Filter className="size-4 text-typography-dark-blue" />
-                <span className="sr-only">Filtro</span>
-              </Button>
+              <Popover modal>
+                <PopoverTrigger asChild>
+                  <Button
+                    className="absolute right-14 top-4 size-auto p-2.5"
+                    variant="outline"
+                    size="icon"
+                  >
+                    <Filter className="size-4 text-typography-dark-blue" />
+                    <span className="sr-only">Filtro</span>
+                    {date?.from && (
+                      <div className="absolute right-1.5 top-1.5 size-1 rounded-full bg-typography-dark-blue" />
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col items-center">
+                  <div className="flex w-full items-center justify-between rounded-lg border px-2 py-1 text-sm">
+                    <CalendarIcon />
+                    {date?.from ? (
+                      date.to ? (
+                        <span className="cursor-default">
+                          {formatDate(date.from, 'dd MMM, y', {
+                            locale: ptBR,
+                          })}{' '}
+                          -{' '}
+                          {formatDate(date.to, 'dd MMM, y', {
+                            locale: ptBR,
+                          })}
+                        </span>
+                      ) : (
+                        formatDate(date.from, 'dd MMM, y', {
+                          locale: ptBR,
+                        })
+                      )
+                    ) : (
+                      <span className="cursor-default text-muted-foreground">
+                        Selecione uma data
+                      </span>
+                    )}
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="size-9"
+                      onClick={() => setDate(undefined)}
+                    >
+                      <X className="size-4" />
+                    </Button>
+                  </div>
+                  <Calendar
+                    mode="range"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                    locale={ptBR}
+                  />
+                </PopoverContent>
+              </Popover>
               <DialogHeader>
                 <DialogTitle className="font-medium leading-3.5 text-typography-dark-blue">
                   Todos os medicamentos administrados
                 </DialogTitle>
               </DialogHeader>
-              <div className="mt-4 flex flex-col gap-4">
-                {Object.entries(groups).map(([data, items], index) => (
+              <div className="mt-4 flex w-full flex-col gap-4">
+                {filteredData.map(([data, items], index) => (
                   <div key={index} className="flex flex-col">
                     <div className="flex items-center gap-2">
                       <div
@@ -140,6 +233,12 @@ export function CardAdministeredMedicinesSection({
                     )}
                   </div>
                 ))}
+
+                {filteredData.length === 0 && (
+                  <span className="text-center text-muted-foreground">
+                    Nenhum registro encontrado.
+                  </span>
+                )}
               </div>
             </DialogContent>
           </Dialog>
