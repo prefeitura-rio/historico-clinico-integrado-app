b67d42af4645be044a077931a19e5ad121f94db2
Victor Assis <victor_assis@poli.ufrj.br>
2025-01-16 14:18:14 -0300

    feat: :sparkles: Adiciona unidades de medida nos parâmetros biométricos do paciênte

--- src/app/(app)/[id]/componentes/episodes/components/episode/components/card-sections/components/card-measures-section.tsx
+++ src/app/(app)/[id]/componentes/episodes/components/episode/components/card-sections/components/card-measures-section.tsx
@@ -9,22 +9,62 @@ interface CardSectionProps {
 export function CardMeasuresSection({ measures }: CardSectionProps) {
   const data = useMemo(
     () => ({
-      Altura: measures?.height,
-      Peso: measures?.weight,
-      IMC: measures?.bmi,
-      'Circunferência abdominal': measures?.abdominal_circumference,
-      Temperatura: measures?.temperature,
-      'Frequência cardíaca': measures?.heart_rate,
-      'Frequência de Pulso': measures?.pulse_rate,
-      'Frequência respiratória': measures?.respiratory_rate,
-      'Hemoglobina glicada': measures?.glycated_hemoglobin,
-      Glicemia: measures?.blood_glucose,
-      'Pressão Sistótica': measures?.systolic_pressure,
-      'Pressão Diastólica': measures?.diastolic_pressure,
-      'Saturação de Oxigênio': measures?.oxygen_saturation,
+      Altura: {
+        value: measures?.height,
+        unit: 'cm',
+      },
+      Peso: {
+        value: measures?.weight,
+        unit: 'kg',
+      },
+      IMC: {
+        value: measures?.bmi,
+        unit: 'kg/m²',
+      },
+      'Circunferência abdominal': {
+        value: measures?.abdominal_circumference,
+        unit: 'cm',
+      },
+      Temperatura: {
+        value: measures?.temperature,
+        unit: '°C',
+      },
+      'Frequência cardíaca': {
+        value: measures?.heart_rate,
+        unit: 'bpm',
+      },
+      'Ritmo de Pulso': {
+        value: measures?.pulse_rate,
+        unit: null,
+      },
+      'Frequência respiratória': {
+        value: measures?.respiratory_rate,
+        unit: 'rpm',
+      },
+      'Hemoglobina glicada': {
+        value: measures?.glycated_hemoglobin,
+        unit: '%',
+      },
+      Glicemia: {
+        value: measures?.blood_glucose,
+        unit: 'mg/dL',
+      },
+      'Pressão Sistólica': {
+        value: measures?.systolic_pressure,
+        unit: 'mmHg',
+      },
+      'Pressão Diastólica': {
+        value: measures?.diastolic_pressure,
+        unit: 'mmHg',
+      },
+      'Saturação de Oxigênio': {
+        value: measures?.oxygen_saturation,
+        unit: '%',
+      },
     }),
     [measures],
   )
+
   return (
     <div
       className="flex cursor-default flex-col gap-2 rounded-lg border bg-card px-6 py-3"
@@ -33,16 +73,19 @@ export function CardMeasuresSection({ measures }: CardSectionProps) {
       <p className="text-sm font-medium text-typography-dark-blue">
         Parâmetros Biométricos
       </p>
-      {Object.values(data).every((value) => !value) ? (
+      {Object.values(data).every((item) => !item.value) ? (
         <span className="text-sm text-typography-blue-gray-200">
           Não há registro de informações
         </span>
       ) : (
         <div className="grid grid-cols-4">
-          {Object.entries(data).map(([key, value], index) =>
-            value ? (
+          {Object.entries(data).map(([key, item], index) =>
+            item.value ? (
               <p key={index} className="text-sm text-typography-blue-gray-200">
-                {key}: <span>{value}</span>
+                {key}:{' '}
+                <span>
+                  {item.value} {item.unit}
+                </span>
               </p>
             ) : null,
           )}
