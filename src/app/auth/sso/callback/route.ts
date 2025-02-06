946e5b88566c7249e4518fdfcadb97052c3a0313
Pedro Marques <pedrovitor.marques@gmail.com>
2025-02-05 23:08:51 -0300

    feat: Implement token retrieval and storage for Gov.br SSO callback

--- src/app/auth/sso/callback/route.ts
+++ src/app/auth/sso/callback/route.ts
@@ -4,6 +4,7 @@ import { cookies } from 'next/headers'
 import { getToken } from '@/http/auth/sso/get-token-in-gov-br'
 
 import { STATE_COOKIE, CODE_VERIFIER_COOKIE } from '@/lib/gov-br'
+import { ACCESS_TOKEN_COOKIE } from '@/lib/api'
 
 
 export async function GET(request: Request) {
@@ -27,10 +28,25 @@ export async function GET(request: Request) {
     return NextResponse.redirect(new URL("/", env.NEXT_PUBLIC_HCI_API_URL));
   }
 
-  // Acquire the token
-  // const tokenResponse = await getToken({code, codeVerifier});
-  // console.log(tokenResponse);
+  // Call HCI API and get the token
+  const tokenResponse = await fetch(`${env.NEXT_PUBLIC_HCI_API_URL}/auth/govbr/login`, {
+    method: "POST",
+    body: JSON.stringify({ code, state, codeVerifier }),
+  });
+
+  if (!tokenResponse.ok) {
+    console.error("Failed to get token");
+  }else{
+    const result = await tokenResponse.json()
+    const token = result.access_token;
+    const expirationTime = result.expires_in;
+    console.log(token, expirationTime);
+
+    cookieStore.set(ACCESS_TOKEN_COOKIE, token, {
+      path: '/',
+      expires: new Date(expirationTime),
+    })
+  }
 
   return NextResponse.redirect(new URL("/", env.NEXT_PUBLIC_HCI_API_URL));
-
 }
