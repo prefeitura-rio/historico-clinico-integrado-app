cd4134d3b3347a8bc74b7bd18c59539a6dc0ca9f
Pedro <pedrovitor.marques@gmail.com>
2025-03-24 13:09:38 -0300

    fix: correct logout redirect URL formatting in logout utility

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -16,7 +16,7 @@ export async function logout() {
   cookieStore.delete(ACCESS_TOKEN_COOKIE);
   cookieStore.delete(ACCESS_TOKEN_EXPIRATION_DATE_COOKIE);
   
-  console.log('Logout in the service:', `${env.NEXT_PUBLIC_URL_PROVIDER}/logout?post_logout_redirect_uri=${env.NEXT_PUBLIC_URL_SERVICE}/auth/sign-in`)
+  console.log('Logout in the service:', `${env.NEXT_PUBLIC_URL_PROVIDER}/logout?post_logout_redirect_uri=${env.NEXT_PUBLIC_URL_SERVICE}auth/sign-in`)
 
-  redirect(`${env.NEXT_PUBLIC_URL_PROVIDER}/logout?post_logout_redirect_uri=${env.NEXT_PUBLIC_URL_SERVICE}/auth/sign-in`)
+  redirect(`${env.NEXT_PUBLIC_URL_PROVIDER}/logout?post_logout_redirect_uri=${env.NEXT_PUBLIC_URL_SERVICE}auth/sign-in`)
 }
