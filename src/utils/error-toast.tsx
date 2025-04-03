a0a29f7f178e4fc575a29ec565a503d93a6818c4
Pedro <pedrovitor.marques@gmail.com>
2025-04-03 14:44:33 -0300

    fix: remove unused noAccessToast logic in ErrorToast component to streamline error handling

--- src/utils/error-toast.tsx
+++ src/utils/error-toast.tsx
@@ -5,7 +5,6 @@ import { toast } from 'sonner'
 
 import {
   GRANT_ERROR_TOAST_COOKIE_KEY,
-  NO_ACCESS_COOKIE_KEY,
   TOO_MANY_REQUESTS_ERROR_TOAST_COOKIE_KEY,
 } from '@/lib/api'
 
@@ -22,12 +21,6 @@ export function ErrorToast() {
   if (grantErrorToast) {
     deleteCookie(GRANT_ERROR_TOAST_COOKIE_KEY)
     toast.error(grantErrorToast, { duration: Infinity, id: 'expired_token' })
-  }
-
-  const noAccessToast = getCookie(NO_ACCESS_COOKIE_KEY)
-  if (noAccessToast) {
-    deleteCookie(NO_ACCESS_COOKIE_KEY)
-    toast.error(noAccessToast, { duration: Infinity, id: 'no_access' })
   }
 
   return null
