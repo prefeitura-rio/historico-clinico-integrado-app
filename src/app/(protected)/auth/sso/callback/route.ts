25a842875d996706f0020bd080839f0db5cc2177
Pedro <pedrovitor.marques@gmail.com>
2025-04-03 14:36:48 -0300

    fix: remove unused NO_ACCESS_COOKIE_KEY import in SSO callback route to clean up code

--- src/app/(protected)/auth/sso/callback/route.ts
+++ src/app/(protected)/auth/sso/callback/route.ts
@@ -3,7 +3,7 @@ import { getEnv } from '@/env/server'
 import { cookies } from 'next/headers'
 
 import { STATE_COOKIE, CODE_VERIFIER_COOKIE } from '@/lib/gov-br'
-import { ACCESS_TOKEN_COOKIE, ACCESS_TOKEN_EXPIRATION_DATE_COOKIE, NO_ACCESS_COOKIE_KEY, api } from '@/lib/api'
+import { ACCESS_TOKEN_COOKIE, ACCESS_TOKEN_EXPIRATION_DATE_COOKIE, api } from '@/lib/api'
 
 
 export async function GET(request: Request) {
