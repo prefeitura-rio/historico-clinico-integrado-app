b8cb5e081cdf1cbf77dc5e6180594ec7301c6989
Pedro <pedrovitor.marques@gmail.com>
2025-04-04 08:23:53 -0300

    fix: update Tally widget identifier in FeedbackButton component

--- src/components/custom-ui/feedback-button.tsx
+++ src/components/custom-ui/feedback-button.tsx
@@ -11,7 +11,7 @@ const FeedbackButton: React.FC<FeedbackButtonProps> = ({ onClick }) => {
     <button
       onClick={onClick}
       className="fixed top-1/2 right-0 -translate-y-1/2 -translate-x-4 origin-right -rotate-90 bg-blue-600 text-white px-4 py-2 rounded-t-md shadow-lg hover:bg-blue-700 transition-all z-50"
-      data-tally-open="mDaOdl"
+      data-tally-open="mY9zqd"
     >
       Feedback
     </button>
