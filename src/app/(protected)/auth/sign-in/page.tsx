540b7d72fc32cddfa4405e6ff64f7cf988fafea8
Pedro <pedrovitor.marques@gmail.com>
2025-04-07 09:27:22 -0300

    refactor: remove outdated user notice from SignIn component to streamline UI

--- src/app/(protected)/auth/sign-in/page.tsx
+++ src/app/(protected)/auth/sign-in/page.tsx
@@ -34,15 +34,6 @@ export default function SignIn() {
             />
           </div>
           <IsActiveForm />
-          <Card className="mx-auto translate-y-5 border-typography-blue-gray-200 bg-dark-gray bg-opacity-[0.08] p-6 text-xs leading-5 text-typography-blue-gray-200">
-            <p className="text-center text-sm font-semibold leading-6 tracking-tight text-primary">
-              Aviso
-            </p>
-              <p>
-                Caro usuário(a), para melhor atendê-lo(a), a partir de 15/03/2025, o acesso ao sistema será feito exclusivamente por meio do portal GOV.BR. 
-                Contate seu gestor para mais informações.
-              </p>
-            </Card>
         </div>
       </div>
     </div>
